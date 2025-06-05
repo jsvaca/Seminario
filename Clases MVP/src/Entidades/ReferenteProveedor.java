91e0f22633ad0e7f4d1a8659940cbd231e07f225
jsvaca <jsvaca2016@gmail.com>
2025-06-05 19:35:10 -0300

    Create ReferenteProveedor.java

---
+++ Clases MVP/src/Entidades/ReferenteProveedor.java
@@ -0,0 +1,9 @@
+package Entidades;
+
+public class ReferenteProveedor extends Referente{
+
+    public ReferenteProveedor(int codigo, String nombre, String email, String telefono) {
+        super(codigo, nombre, email, telefono);
+    }
+
+}
